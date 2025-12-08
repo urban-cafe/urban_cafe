1505a835d31e1e73cf00c23e7c5fda60131c5248
aungheinhtet <aungheinhtet380@gmail.com>
2025-12-08 20:31:13 +0630

    refactor(widgets): simplify add category dialog layout by removing unnecessary scroll view

--- lib/presentation/widgets/add_category_dialog.dart
+++ lib/presentation/widgets/add_category_dialog.dart
@@ -6,32 +6,29 @@ import 'package:urban_cafe/presentation/providers/admin_provider.dart';
 Future<String?> showAddCategoryDialog(BuildContext context, {String? parentId}) async {
   final isMain = parentId == null;
   final ctrl = TextEditingController();
-  final formKey = GlobalKey<FormState>(); // Key to identify the form
+  final formKey = GlobalKey<FormState>();
 
   return await showDialog<String?>(
     useSafeArea: true,
     context: context,
     builder: (ctx) => AlertDialog(
       title: Text(isMain ? 'New Main Category' : 'New Sub Category'),
-      content: SingleChildScrollView(
-        child: Form(
-          key: formKey,
-          child: TextFormField(
-            controller: ctrl,
-            autofocus: true,
-            textCapitalization: TextCapitalization.words,
-            scrollPadding: const EdgeInsets.only(bottom: 200),
-            decoration: const InputDecoration(labelText: 'Name', border: OutlineInputBorder()),
-            // USE GLOBAL VALIDATOR
-            validator: (value) => AppValidators.required(value, 'Name'),
-          ),
+      content: Form(
+        key: formKey,
+        child: TextFormField(
+          controller: ctrl,
+          autofocus: true,
+          textCapitalization: TextCapitalization.words,
+          scrollPadding: const EdgeInsets.only(bottom: 200),
+          decoration: const InputDecoration(labelText: 'Name', border: OutlineInputBorder()),
+
+          validator: (value) => AppValidators.required(value, 'Name'),
         ),
       ),
       actions: [
         TextButton(onPressed: () => Navigator.pop(ctx, null), child: const Text('Cancel')),
         FilledButton(
           onPressed: () async {
-            // CHECK VALIDATION
             if (!formKey.currentState!.validate()) return;
 
             final id = await ctx.read<AdminProvider>().addCategory(ctrl.text.trim(), parentId: parentId);
